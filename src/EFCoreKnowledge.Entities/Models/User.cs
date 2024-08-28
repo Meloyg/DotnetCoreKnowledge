6d018c066df9764784136f80e96122718a0ad07a
Melo <melo.guan@gentrack.com>
2024-08-28 23:47:29 +1200

    update model

--- src/EFCoreKnowledge.Entities/Models/User.cs
+++ src/EFCoreKnowledge.Entities/Models/User.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using EFCoreKnowledge.Entities.Interfaces;
 
 namespace EFCoreKnowledge.Entities.Models;
 
+[Table("Users")]
 public class User : IAuditableEntity
 {
+    [Key]
     public long Id { get; protected set; }
+    
+    [Required]
     public string Name { get; set; }
+    
+    [Required]
     public string Email { get; set; }
     public DateTime Created { get; private set; }
     public DateTime Modified { get; private set; }
